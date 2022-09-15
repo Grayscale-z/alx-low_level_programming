17df54c97836f36ad8056e7ce444ec236bfb36ed
King <ndubuzor@gmail.com>
2022-09-14 17:37:31 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,19 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
-
  * print_to_98 - prints all the integers between the number given and 98
-
  * @n: number to count from
-
  * Return: void
-
  */
 
 void print_to_98(int n)
 
 {
-	if (n > 98)
+if (n > 98)
 {
 
 while (n >= 98)
