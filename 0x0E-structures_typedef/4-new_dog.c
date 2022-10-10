ba3ff909238636925501f1d7f083e6f761455eb4
King <ndubuzor@gmail.com>
2022-10-10 15:07:55 -0700

    All tasks committed at once

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,8 @@ return (len);
 }
 
 /**
- * _strcpy - Copies a string pointed to by src, including the terminating null byte, to a buffer pointed to by dest
+ * _strcpy - Copies a string pointed to by src,
+ * including the terminating null byte, to a buffer pointed to by dest
  * @dest: The buffer storing the string copy
  * @src: The source string
  *
